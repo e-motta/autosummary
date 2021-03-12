48b6f213294a58daef379f732f313534ecec2b56
Eduardo Moraes <edumoraes@live.com>
2021-03-12 14:37:51 -0300

    update source, fix nltk.txt

--- summarizer.py
+++ summarizer.py
@@ -20,10 +20,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-import nltk
-
-
-nltk.download('punkt')
 
 # Available languages
 
